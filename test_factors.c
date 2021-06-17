cb04756792ab861b07f921a511c8f3b24aea3b7c
Gopikant kumar <76965991+Gopi-2001@users.noreply.github.com>
2021-06-17 12:18:36 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#include <stdbool.h>
 
+int factors(int n, int arr[]){
+int sq_root = n/2;
+  int count  = 0;
+  for(int i = 2; i <= sq_root; i++){
+  bool flag = true;
+    if(n == 1){
+    break;
+    }
+    
+    for(int j = 2 ;j < i;j++)
+    {
+    if((i %j )==0)
+    {
+      flag =false;
+      break;
+    }
+    } 
+    if(flag)
+    {
+    while( n%i == 0)
+    {
+    arr[count] = i;
+    count++;
+    n = n/i;
+    }
+    }
+  }
+return (count);
 
-int factors(int, int []);
+}
 
 int main(void) {
   int ret[100] = {0};
@@ -19,5 +49,6 @@ int main(void) {
   assert (count == 2);
   assert (ret[0] == 11);
   assert (ret[1] == 13);
-  printf("Factors: passed\n");
+ 
+ printf("Factors: passed\n");
 }
